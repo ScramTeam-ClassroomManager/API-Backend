33583cb728bcfdc30e5d923cc2c4af4b6488f700
Luigi Villella <villellaluigi@yahoo.it>
2024-10-27 18:57:20 +0100

    SCRUM-8:add springboot project
    - Initialized Spring Boot project with basic configurations
    - Set up application.properties with default database settings

---
+++ src/main/java/it/unical/classroommanager_api/ClassroomManagerApiApplication.java
@@ -0,0 +1,13 @@
+package it.unical.classroommanager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomManagerApiApplication.class, args);
+	}
+
+}

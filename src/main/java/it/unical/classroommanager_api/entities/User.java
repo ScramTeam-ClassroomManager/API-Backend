92a050484f15343fe5b0579198f9298e74f830bc
Luigi Villella <villellaluigi@yahoo.iit>
2024-10-28 17:43:45 +0100

    SCRUM-14:User entity
    -added user entity
    -add application.properties in gitignore

---
+++ src/main/java/it/unical/classroommanager_api/entities/User.java
@@ -0,0 +1,34 @@
+package it.unical.classroommanager_api.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.UuidGenerator;
+
+@Data
+@Entity
+@Table(name="USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", nullable = false)
+    private long id;
+
+    @UuidGenerator()
+    @Column(name = "SERIAL_NUMBER",unique = true,nullable = false)
+    private String serialNumber;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name="EMAIL",unique = true)
+    private String email;
+
+    @Column(name = "PASSWORD")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+}

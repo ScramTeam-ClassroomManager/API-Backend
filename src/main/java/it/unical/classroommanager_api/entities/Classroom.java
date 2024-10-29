ecfc7b5957c37ada5382751d04fc1411138834ef
serrux <srrfnc02b19d122v@studenti.unical.it>
2024-10-29 12:29:58 +0100

    Create classroom class

---
+++ src/main/java/it/unical/classroommanager_api/entities/Classroom.java
@@ -0,0 +1,11 @@
+package it.unical.classroommanager_api.entities;
+
+public class Classroom {
+    private int id;
+    private String name;
+    private int cube;
+    private int floor;
+    private int capability;
+    private int nPrese;
+    private boolean projector;
+}

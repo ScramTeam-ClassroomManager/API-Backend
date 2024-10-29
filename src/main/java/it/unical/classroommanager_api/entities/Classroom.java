3699f1cf9becbe865006a8ac354139cc9cdebd84
serrux <srrfnc02b19d122v@studenti.unical.it>
2024-10-29 12:36:34 +0100

    Modified classroom class

--- src/main/java/it/unical/classroommanager_api/entities/Classroom.java
+++ src/main/java/it/unical/classroommanager_api/entities/Classroom.java
@@ -1,11 +1,33 @@
 package it.unical.classroommanager_api.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="CLASSROOMS")
 public class Classroom {
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", nullable = false)
+    private long id;
+
+    @Column(name = "NAME", nullable = false, unique = true)
     private String name;
+
+    @Column(name = "CUBE", nullable = false)
     private int cube;
+
+    @Column(name = "FLOOR", nullable = false)
     private int floor;
+
+    @Column(name = "CAPABILITY", nullable = false)
     private int capability;
-    private int nPrese;
+
+    @Column(name = "NUM_SOCKET")
+    private int numSocket;
+
+    @Column(name = "PROJECTOR")
     private boolean projector;
 }

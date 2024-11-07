b09fa218e45fc9b6d997174923ba499a2cc1ae4d
serrux <125915660+serrux@users.noreply.github.com>
2024-11-07 18:58:02 +0100

    Update ClassroomManagerApiApplicationTests.java

--- src/test/java/it/unical/classroommanager_api/ClassroomManagerApiApplicationTests.java
+++ src/test/java/it/unical/classroommanager_api/ClassroomManagerApiApplicationTests.java
@@ -7,9 +7,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ClassroomManagerApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-		assertEquals(1,4);
-	}
-
 }

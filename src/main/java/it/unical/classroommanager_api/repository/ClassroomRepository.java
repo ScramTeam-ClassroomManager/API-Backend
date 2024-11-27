65fdb7fb49f1d52dd45d3405b1a3435231caea60
PeppeLe21 <giuseppe.lentini1021@gmail.com>
2024-11-27 15:21:22 +0100

    Modifiche per gestire la nuova schermata

--- src/main/java/it/unical/classroommanager_api/repository/ClassroomRepository.java
+++ src/main/java/it/unical/classroommanager_api/repository/ClassroomRepository.java
@@ -1,12 +1,15 @@
 package it.unical.classroommanager_api.repository;
 
 import it.unical.classroommanager_api.entities.Classroom;
+import it.unical.classroommanager_api.entities.Cube;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
     Optional<Classroom> findClassroomById(Long id);
+    List<Classroom> findByCube(Cube cube);
 }
